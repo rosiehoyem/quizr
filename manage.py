dec4f4db5a3116a4c84a25870cd72f05d8dd653f
Rosie Hoyem <rosiehoyem@gmail.com>
2017-02-11 15:00:29 -0600

    added runtime and manage file

---
+++ manage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
